0aeeddfef8f97526f74c5a6e348782ff18ec0eff
rrgks6221 <rrgks@naver.com>
2024-04-28 12:28:54 +0900

    chore: 사용하지 않는 파일 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,4 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from '@src/app.controller';
-import { AppService } from '@src/app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({})
 export class AppModule {}
