69edcd87d266d728546239feb7258379e9173d81
rrgks6221 <rrgks@naver.com>
2024-05-04 12:52:16 +0900

    chore: global app settings
    글로벌 파이프 설정
    글로벌 인터셉터 설정
    스웨거 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from '@src/app.module';
+import {
+  createApp,
+  setGlobalInterceptor,
+  setGlobalPipe,
+  setSwagger,
+} from '@src/bootstrap';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await createApp();
+
+  setGlobalPipe(app);
+  setGlobalInterceptor(app);
+  setSwagger(app);
+
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT, () => {
+    console.info(`Server listening on port ${PORT}`);
+  });
 }
 bootstrap();
