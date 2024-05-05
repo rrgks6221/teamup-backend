fc46ee1ad0e477a42a43c4d1a676319b76e15584
rrgks6221 <rrgks@naver.com>
2024-05-05 17:37:52 +0900

    feat: 환경에 대한 처리를 담당하는 AppConfigService추가
    테스트환경에서 다른 환경변수를 사용할 수 있게 설정함
    테스트환경에서 절대경로로 임포트된 소스코드를 읽을 수 있게 설정을 추가

---
+++ src/common/app-config/app-config.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { ENV_KEY } from '@src/common/app-config/app-config.constant';
+
+type EnvKey = keyof typeof ENV_KEY;
+
+@Injectable()
+export class AppConfigService {
+  constructor(private readonly configService: ConfigService) {}
+
+  get<T extends string | number = string>(key: EnvKey): T {
+    return this.configService.get<T>(key) as T;
+  }
+}
