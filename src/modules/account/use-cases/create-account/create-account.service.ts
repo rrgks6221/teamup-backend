e2464c490d8d584eddb3d372eb36d0f2f34c80ad
rrgks6221 <rrgks@naver.com>
2024-05-26 22:09:54 +0900

    refactor: 비밀번호 암호화 로직 account 도메인에서 수행하도록 변경

--- src/modules/account/use-cases/create-account/create-account.service.ts
+++ src/modules/account/use-cases/create-account/create-account.service.ts
@@ -1,5 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 
+import bcrypt from 'bcrypt';
+
 import { Account } from '@module/account/entities/account.entity';
 import { AccountNicknameAlreadyOccupiedError } from '@module/account/errors/account-nickname-already-occupied.error';
 import { AccountUsernameAlreadyOccupiedError } from '@module/account/errors/account-username-already-occupied.error';
@@ -22,7 +24,7 @@ export class CreateAccountService implements ICreateAccountService {
   async execute(command: CreateAccountCommand): Promise<Account> {
     const newAccount = Account.createByUsername({
       username: command.username,
-      password: command.password,
+      password: await bcrypt.hash(command.password, 10),
       signInType: command.signInType,
       nickname: command.nickname,
     });
