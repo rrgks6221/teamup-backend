21d00d0e561eaea9f23fa84d839501587130b580
rrgks6221 <rrgks@naver.com>
2024-06-01 14:22:48 +0900

    feat: username 기반 로그인 기능

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { CreateAccountModule } from '@module/account/use-cases/create-account/create-account.module';
 import { DeleteAccountModule } from '@module/account/use-cases/delete-account/delete-account.module';
+import { GetAccountByUsernameModule } from '@module/account/use-cases/get-account-by-username/get-account-by-username.module';
 import { GetAccountModule } from '@module/account/use-cases/get-account/get-account.module';
 import { UpdateAccountModule } from '@module/account/use-cases/update-account/update-account.module';
 
@@ -10,7 +11,9 @@ import { UpdateAccountModule } from '@module/account/use-cases/update-account/up
     CreateAccountModule,
     DeleteAccountModule,
     GetAccountModule,
+    GetAccountByUsernameModule,
     UpdateAccountModule,
   ],
+  exports: [GetAccountByUsernameModule],
 })
 export class AccountModule {}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { SignInWithUsernameModule } from '@module/auth/use-cases/sign-in-with-username/sign-in-with-username.module';
 import { SignUpWithUsernameModule } from '@module/auth/use-cases/sign-up-with-username/sign-up-with-username.module';
 
 @Module({
-  imports: [SignUpWithUsernameModule],
+  imports: [SignUpWithUsernameModule, SignInWithUsernameModule],
 })
 export class AuthModule {}
