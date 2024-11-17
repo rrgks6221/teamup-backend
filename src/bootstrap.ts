be334637ede946b5681cded8e5776e556d810e36
rrgks6221 <rrgks@naver.com>
2024-11-17 15:33:41 +0900

    chore: global app settings
    글로벌 파이프 설정
    글로벌 인터셉터 설정
    스웨거 설정

---
+++ src/bootstrap.ts
@@ -0,0 +1,58 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+  ValidationPipeOptions,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from '@src/app.module';
+
+export const createApp = async () => {
+  return await NestFactory.create(AppModule);
+};
+
+export const setGlobalPipe = (app: INestApplication) => {
+  const options: Omit<ValidationPipeOptions, 'exceptionFactory'> = {
+    transform: true,
+    whitelist: true,
+  };
+
+  app.useGlobalPipes(new ValidationPipe(options));
+};
+
+export const setGlobalInterceptor = (app: INestApplication) => {
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+};
+
+export const setSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('era-meet')
+    .setDescription('The era-meet API description')
+    .setVersion('0.1')
+    .addTag('era-meet')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+      operationsSorter: (a: Map<string, string>, b: Map<string, string>) => {
+        const order = {
+          post: '0',
+          get: '1',
+          put: '2',
+          patch: '3',
+          delete: '4',
+        };
+
+        return order[a.get('method') as string].localeCompare(
+          order[b.get('method') as string],
+        );
+      },
+    },
+  });
+};
