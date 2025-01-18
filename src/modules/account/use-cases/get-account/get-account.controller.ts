99bd143b4b41a67c8cf557dd397c8ccc053da7a0
rrgks6221 <rrgks@naver.com>
2025-01-18 12:35:52 +0900

    chore: 내 계정 조회 API를 계정 단일조회 유즈케이스 디렉토리에 응집
    라우팅 순서가 중요하고 엔드포인트만 다를 뿐 유즈케이스에 대한 동작은 동일하기때문에

--- src/modules/account/use-cases/get-account/get-account.controller.ts
+++ src/modules/account/use-cases/get-account/get-account.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, HttpStatus, Inject, Param } from '@nestjs/common';
 import {
+  Controller,
+  Get,
+  HttpStatus,
+  Inject,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
@@ -14,10 +22,19 @@ import {
   GetAccountQuery,
   IGetAccountService,
 } from '@module/account/use-cases/get-account/get-account.service.interface';
+import { JwtAuthGuard } from '@module/auth/jwt/jwt-auth.guard';
 
 import { BaseHttpException } from '@common/base/base-http-exception';
+import {
+  CurrentUser,
+  ICurrentUser,
+} from '@common/decorator/current-user.decorator';
 
 @ApiTags('account')
+@ApiOkResponse({ type: AccountResponseDto })
+@ApiNotFoundResponse({
+  schema: BaseHttpException.buildSwaggerSchema([AccountNotFoundError.CODE]),
+})
 @Controller()
 export class GetAccountController {
   constructor(
@@ -25,11 +42,29 @@ export class GetAccountController {
     private readonly getAccountService: IGetAccountService,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '내 계정 조회' })
+  @ApiBearerAuth()
+  @Get('accounts/me')
+  async getCurrentAccount(
+    @CurrentUser() currentUser: ICurrentUser,
+  ): Promise<AccountResponseDto> {
+    try {
+      const query = new GetAccountQuery({ id: currentUser.id });
+
+      const account = await this.getAccountService.execute(query);
+
+      return AccountDtoAssembler.convertToDto(account);
+    } catch (error) {
+      if (error instanceof AccountNotFoundError) {
+        throw new BaseHttpException(HttpStatus.NOT_FOUND, error);
+      }
+
+      throw error;
+    }
+  }
+
   @ApiOperation({ summary: '특정 계정 조회' })
-  @ApiOkResponse({ type: AccountResponseDto })
-  @ApiNotFoundResponse({
-    schema: BaseHttpException.buildSwaggerSchema([AccountNotFoundError.CODE]),
-  })
   @Get('accounts/:id')
   async getAccount(@Param('id') id: string): Promise<AccountResponseDto> {
     try {
